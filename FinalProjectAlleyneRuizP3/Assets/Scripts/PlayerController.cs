b2d8e5be34250eae32477072f58f77d57f1542a3
Diego Alleyne <714289@student.dusd.net>
2024-05-29 11:40:16 -0700

    Camera Movement
    Continued to work on camera movement to follow player 5/29

--- FinalProjectAlleyneRuizP3/Assets/Scripts/PlayerController.cs
+++ FinalProjectAlleyneRuizP3/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.SceneView;
 
 public class PlayerController : MonoBehaviour
 {
+    CharacterController Controller;
     public float horizontalInput;
     public float verticalInput;
     public float speed = 10.0f;
+
+    private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal"); 
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = input.x * speed * transform.right + input.y * speed * transform.forward;
+    }
+
+    private void This()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward *  verticalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
