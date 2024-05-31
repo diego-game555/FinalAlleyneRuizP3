5b2b6d4f36ba68874eecc41ec2eb478b059ea907
MiaR730 <723420@student.dusd.net>
2024-05-30 23:11:42 -0700

    UI and background
    Added Score UI. Added trees, rocks, Mts, ground texture and added texture to the castle.

--- FinalProjectAlleyneRuizP3/Assets/Scripts/GameManager.cs
+++ FinalProjectAlleyneRuizP3/Assets/Scripts/GameManager.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public int health;
+    public TextMeshProUGUI scoreText;
+
+    private int score;
+   
     // Start is called before the first frame update
     void Start()
     {
         health = 100;
         Updatehealth(0);
+
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -23,5 +31,11 @@ public class GameManager : MonoBehaviour
     {
         health += healthToAdd;
         
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score:" + score;
     }
 }
